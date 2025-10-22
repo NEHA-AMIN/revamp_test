e184132549cc5411b4a7e7d34e543ba794768f0b
kroolo-anuj <anuj.dwivedi@kroolo.com>
2025-10-23 04:29:17 +0530

    ok

---
+++ .next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/7a51731df2ab9baa.js",
+    "static/chunks/fee78cc6e50fdad5.js",
+    "static/chunks/8c868723bd09d263.js",
+    "static/chunks/01bd51e4ce3f19a7.js",
+    "static/chunks/turbopack-7b8e24e1bd03ccfa.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
